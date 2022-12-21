4ea6207e390b8985b210d670b934a811db5c0cab
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2022-12-21 19:11:32 +0200

    Day3, part2

--- day3/main.go
+++ day3/main.go
@@ -8,6 +8,10 @@ import (
 )
 
 func main() {
+	part2()
+}
+
+func part1() {
 	data, err := os.ReadFile("day3/input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -37,4 +41,52 @@ func main() {
 		}
 	}
 	fmt.Println(sum)
+}
+
+func part2() {
+	data, err := os.ReadFile("day3/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	rucksacks := strings.Split(string(data), "\n")
+	var badges []rune
+	for _, group := range chunks(rucksacks, 3) {
+		set1 := map[rune]bool{}
+		for _, item1 := range group[0] {
+			set1[item1] = true
+		}
+		common12 := map[rune]bool{}
+		for _, item2 := range group[1] {
+			if set1[item2] {
+				common12[item2] = true
+			}
+		}
+		for _, item3 := range group[2] {
+			if common12[item3] {
+				badges = append(badges, item3)
+				break
+			}
+		}
+	}
+	var sum int32
+	for _, badge := range badges {
+		if badge < 'a' {
+			sum += badge - 38
+		} else {
+			sum += badge - 96
+		}
+	}
+	fmt.Println(sum)
+}
+
+func chunks(slice []string, chunkSize int) [][]string {
+	var res [][]string
+	for i := 0; i < len(slice); i += chunkSize {
+		end := i + chunkSize
+		if end > len(slice) {
+			end = len(slice)
+		}
+		res = append(res, slice[i:end])
+	}
+	return res
 }
